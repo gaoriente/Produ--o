1ff71d674c5d0ce4fda1ce7ae74169272cdcfef0
gaoriente <gabriel.oriente@hotmail.com>
2017-07-10 17:41:53 -0300

    Começando

---
+++ _js/login.js
@@ -0,0 +1,17 @@
+var login = $("#login");
+var password = $("#password");
+
+function teste(){
+
+  var login = $("#login").val();
+  var password = $("#password").val();
+        if (login=="admin" && password=="admin") {
+            window.location = "../_html/administrador_inicio.html";
+        }else if (login=="boy" && password=="boy") {
+            window.location = "../_html/motoboy_inicio.html";
+        }else if (login=="cliente" && password=="cliente") {
+            window.location = "../_html/cliente_contato.html";
+        }else {
+          alert("Login ou senha incorretos!");
+        }
+}
